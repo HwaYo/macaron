3ae5a30a8174738977ccf01e05813e3e4a4b43d7
stompesi <stompesi@gmail.com>
2015-03-14 22:10:39 +0900

    fund show layout add

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -2,4 +2,5 @@ class Fund < ActiveRecord::Base
   belongs_to :user
   belongs_to :gift
   belongs_to :friend
+  has_many :contributions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314091637) do
+ActiveRecord::Schema.define(version: 20150314123615) do
+
+  create_table "contributions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "fund_id"
+    t.integer  "amount"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friends", force: :cascade do |t|
     t.string   "name"
