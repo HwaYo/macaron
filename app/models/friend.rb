8748829c630ef0085e2d8a12d4152c06f8e46512
DevSusu <os1742@gmail.com>
2015-03-15 01:24:08 +0900

    fixed fund show. fixed op_image. kakaostory abled

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,4 +2,8 @@ class Friend < ActiveRecord::Base
   belongs_to :user , dependent: :destroy
   has_many :funds
   validates :uid , uniqueness: true
+
+  def profile_image
+    "http://graph.facebook.com/#{self.uid}/picture"
+  end
 end
