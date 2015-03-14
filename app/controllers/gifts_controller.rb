f20126450845aeea4a529481579b41029f8081fd
minhyeok4dev <minhyeok4dev@gmail.com>
2015-03-14 17:45:28 +0900

    fixed age addtion

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -7,6 +7,6 @@ class GiftsController < ApplicationController
     @friend = Friend.find(params[:friend_id])
     @user = User.find_by_id(@friend.user_id)
     sex = ['common','male', 'female']
-    @gifts = Gift.where("min_age <= ? AND max_age > ?", @user.age+10, @user.age+10).where("gender = 0 OR gender = ?", sex.find_index(@user.gender)).order(:created_at).page(params[:page]).per(8)
+    @gifts = Gift.where("min_age <= ? AND max_age > ?", @user.age, @user.age).where("gender = 0 OR gender = ?", sex.find_index(@user.gender)).order(:created_at).page(params[:page]).per(8)
   end
 end
