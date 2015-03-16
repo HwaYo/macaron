e5b3b1b5d6cfb9880d8280fb14accd0b025828b3
Hong ChulJu <ang0123dev@gmail.com>
2015-03-16 23:15:10 +0900

    some tags are moved to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def number_to_won(number)
+    number_to_currency number, unit: '원', precision: 0, format: '%n%u'
+  end
+
+  def facebook_image_tag_by_uid(uid, *args, &block)
+    options = args.extract_options!
+    url_options = options.slice(:width, :height)
+    image_url = "//graph.facebook.com/#{uid}/picture?#{url_options.to_query}"
+    image_tag image_url, options, &block
+  end
 end
